d533f9f9bc053abe147a7bce008ce9cd6254809e
muhmubar <muhmubar@MUHMUBAR-T490.eg.oracle.com>
2023-01-14 00:55:22 +0200

    update model

--- model/user.js
+++ model/user.js
@@ -4,9 +4,9 @@ const userSchema = mongoose.Schema(
   {
     userName: { type: String,require:true },
     creationAt: { type: Date ,default:Date.now(),require:true},
-    Gender:  { type:String ,enum:['male','female','others'],require:true},
+    gender:  { type:String ,enum:['male','female','others'],require:true},
     dateOfBirth: { type: String ,require:true},
-    Email: { type: String ,require:true},
+    email: { type: String ,require:true},
     status: { type:String, enum:['active','inactive','frequent'], default:'active',require:true },
     password: { type: String ,require:true},
     token:{ type:String ,require:true},
